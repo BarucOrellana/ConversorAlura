ae627668bab8175ab7c48a82a83e8c8493d5ea24
Baruc Orellana <barucorellana@gmail.com>
2023-04-10 03:25:17 -0600

    corrigiendo valor de moneda Mexicana

--- ConversorMonedas/ConvertirMonedasMexico.java
+++ ConversorMonedas/ConvertirMonedasMexico.java
@@ -5,53 +5,53 @@ import javax.swing.JOptionPane;
 public class ConvertirMonedasMexico {
 
 	public void ConvertirPesosMexicanosDolares(double valor) {
-		double monedaDolar = valor / 137.68;
+		double monedaDolar = valor / 18.11;
 		monedaDolar = (double)Math.round(monedaDolar * 100d) /100 ;
 		JOptionPane.showMessageDialog(null, "Tienes $ " + monedaDolar + " Dolares");
 	}
 	public void ConvertirPesosMexicanosEuros(double valor) {
-		double monedaEuro = valor / 137.20;
+		double monedaEuro = valor / 19.76;
 		monedaEuro = (double)Math.round(monedaEuro * 100d) /100 ;
 		JOptionPane.showMessageDialog(null, "Tienes $ " + monedaEuro + " Euros");
 	}
 	public void ConvertirPesosMexicanosLibra(double valor) {
-		double monedaLibra = valor / 161.52;
+		double monedaLibra = valor / 22.50;
 		monedaLibra = (double)Math.round(monedaLibra * 100d) /100 ;
 		JOptionPane.showMessageDialog(null, "Tienes $ " + monedaLibra + " Libras Esterlinas");
 	}
 	public void ConvertirPesosMexicanosYen(double valor) {
-		double monedaYen = valor / 1;
+		double monedaYen = valor / 0.14;
 		monedaYen = (double)Math.round(monedaYen * 100d) /100 ;
 		JOptionPane.showMessageDialog(null, "Tienes $ " + monedaYen + " Yen Japonés");
 	}
 	public void ConvertirPesosMexicanosWon(double valor) {
-		double monedaWon = valor / 0.1;
+		double monedaWon = valor / 0.014;
 		monedaWon = (double)Math.round(monedaWon * 100d) /100 ;
 		JOptionPane.showMessageDialog(null, "Tienes $ " + monedaWon + " Won sub-coreano");
 	}
 	
 	public void ConvertirDolaresPesosMexicanos(double valor) {
-		double monedaDolar = 137.68 * valor;
+		double monedaDolar = 18.11 * valor;
 		monedaDolar = (double)Math.round(monedaDolar * 100d) /100 ;
 		JOptionPane.showMessageDialog(null, "Tienes Dolares " + monedaDolar + " en pesos");
 	}
 	public void ConvertirEurosPesosMexicanos(double valor) {
-		double monedaEuro = valor * 137.20;
+		double monedaEuro = valor * 19.76;
 		monedaEuro = (double)Math.round(monedaEuro * 100d) /100 ;
 		JOptionPane.showMessageDialog(null, "Tienes Euros " + monedaEuro + " en pesos");
 	}
 	public void ConvertirLibraPesosMexicanos(double valor) {
-		double monedaLibra = valor * 161.52;
+		double monedaLibra = valor * 22.50;
 		monedaLibra = (double)Math.round(monedaLibra * 100d) /100 ;
 		JOptionPane.showMessageDialog(null, "Tienes Libras Esterlinas " + monedaLibra + " en pesos");
 	}
 	public void ConvertirYenPesosMexicanos(double valor) {
-		double monedaYen = valor * 1;
+		double monedaYen = valor * 0.14;
 		monedaYen = (double)Math.round(monedaYen * 100d) /100 ;
 		JOptionPane.showMessageDialog(null, "Tienes Yen Japonés " + monedaYen + " en pesos");
 	}
 	public void ConvertirWonPesosMexicanos(double valor) {
-		double monedaWon = valor * 0.1;
+		double monedaWon = valor * 0.014;
 		monedaWon = (double)Math.round(monedaWon * 100d) /100 ;
 		JOptionPane.showMessageDialog(null, "Tienes Won sub-coreano " + monedaWon + " en pesos");
 	}
